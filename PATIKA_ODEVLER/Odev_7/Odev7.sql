2b01459f72fd4d3e90d6b03bb4f0c410f0b3693d
musakibar <kibar378@gmail.com>
2022-09-12 14:57:38 +0300

    Odev 5,6,7

---
+++ PATIKA_ODEVLER/Odev_7/Odev7.sql
@@ -0,0 +1,8 @@
+SELECT rating FROM film GROUP BY rating;
+
+SELECT replacement_cost FROM film GROUP BY replacement_cost HAVING COUNT(*) > 50;
+
+SELECT store_id, COUNT(*) FROM customer GROUP BY store_id;
+
+SELECT country_id , COUNT(*) FROM city GROUP BY country_id ORDER BY COUNT(*) DESC LIMIT 1;
+
